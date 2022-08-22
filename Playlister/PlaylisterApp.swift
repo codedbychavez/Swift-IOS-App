02bd00e022f7bd15bbc225f88fa660686e844306
codedbychavez <codedbychavez@gmail.com>
2022-08-21 21:12:15 -0300

    Initial Commit

---
+++ Playlister/PlaylisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylisterApp.swift
+//  Playlister
+//
+//  Created by Chavez Harris on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct PlaylisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
